2f56312f98c00c649a6b771964f92f1437b214ed
iamtwobe <danielnm05@gmail.com>
2024-06-20 18:42:38 -0300

    project start

---
+++ currencies_lib/__init__.py
@@ -0,0 +1,43 @@
+from ._currencies import _Currency_Formater
+from datetime import datetime
+
+__version__ = "0.1"
+
+
+
+test = ''
+"""Provide several sample math calculations.
+
+This module allows the user to make mathematical calculations.
+
+The module contains the following functions:
+
+- `add(a, b)` - Returns the sum of two numbers.
+- `subtract(a, b)` - Returns the difference of two numbers.
+- `multiply(a, b)` - Returns the product of two numbers.
+- `divide(a, b)` - Returns the quotient of two numbers.
+"""
+
+def teste() -> None:
+    """Compute and return the sum of two numbers.
+
+    Examples:
+        >>> add(4.0, 2.0)
+        6.0
+        >>> add(4, 2)
+        6.0
+
+    Args:
+        a (float): A number representing the first addend in the addition.
+        b (float): A number representing the second addend in the addition.
+
+    Returns:
+        float: A number representing the arithmetic sum of `a` and `b`.
+    """
+
+# Lógica para o GET da API de maneira assíncrona seguindo a data
+
+_inst = _Currency_Formater()
+BRL = _inst.BRL
+
+del datetime
---
+++ currencies_lib/_currencies.py
@@ -0,0 +1,45 @@
+class _Currency_Formater():
+
+    def __init__(self):
+        self.nof = "Missing value"
+
+    def _value_check(func):
+        def inner(self, value = None, **kwargs):
+            if not value:
+                print('\033[91m', f'ERROR: Missing value for "{__class__.__name__} ({func.__name__})"', '\033[0m')
+                return None
+            function_exec = func(self, value, **kwargs)
+            return function_exec
+        return inner
+    
+    def _currency_data_check(func):
+        def inner(self, value = None, **kwargs):
+            print("!!!!!!!!!")
+        return inner
+
+    @_value_check
+    def BRL(self, value = None, *, teste=False) -> str:
+        # Definição de métodos de uso a partir de kwargs
+        if teste == True:
+            print("test success")
+        print("BRL", value)
+
+        return "NOTFINISHED"
+
+    @_value_check
+    def BRL_to_float(self, value = None) -> float:
+        print("Float", value)
+
+        return 1.2
+
+    @_value_check
+    def USD(self, value = None) -> str:
+        print("USD", value)
+
+        return "NOTFINISHED"
+
+    @_value_check
+    def EUR(self, value = None) -> str:
+        print("EUR", value)
+
+        return "NOTFINISHED"
