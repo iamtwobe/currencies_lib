59f514f3a5c0fac6dffe01509a7fb96e215197e0
iamtwobe <danielnm05@gmail.com>
2024-06-22 19:09:25 -0300

    functions update

--- currencies_lib/_currencies.py
+++ currencies_lib/_currencies.py
@@ -2,11 +2,20 @@ class _Currency_Formater():
 
     def __init__(self):
         self.nof = "Missing value"
+        self.symbols = r" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz$£¢<>?!%&*(){}[]-=+~^|/"
         self.currency_link = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{}.json"
         # ex.: print(self.currency_link.format("usd"))
 
     def _value_check(func):
         def inner(self, value = None, **kwargs):
+            try:
+                if type(value) in (float, int):
+                    pass
+                else:
+                    raise ValueError
+            except ValueError:
+                print(f'Currency Formatter: The inputted value is not a Integer or a Float. ("{type(value)}")')
+                return None
             if not value:
                 print('\033[91m', f'ERROR: Missing value for "{__class__.__name__} ({func.__name__})"', '\033[0m')
                 return None
@@ -23,19 +32,104 @@ class _Currency_Formater():
         return self.currency_link.format(currency)
 
     @_value_check
-    def BRL(self, value = None, *, teste=False) -> str:
-        # Definição de métodos de uso a partir de kwargs
-        if teste == True:
-            print("test success")
-        print("BRL", value)
+    def as_percentage(self, value = None, *, decimals=2, percent_symb=True) -> str:
+        try:
+            final_value = f"{float(value) * 100:.{decimals}f}{"%" if percent_symb == True else ""}"
 
-        return "NOTFINISHED"
+            return final_value
+        except Exception as e:
+            print(e)
+        
+        return None
 
     @_value_check
-    def BRL_to_float(self, value = None) -> float:
-        print("Float", value)
+    def custom_format(self, value = None, *, currency_sign: str=None, 
+            thousands_sep: str=',', decimal_sep: str='.',
+            custom_format=None) -> str:
+        
+        if not custom_format:
+            print(f'Currency Formatter: Format must be provided.')
+            return None
+        if type(custom_format) != str:
+            print(f'Currency Formatter: The inputted format is not a String type. ("{type(custom_format)}")')
+            return None
+        if len(custom_format) < 1:
+            print(f'Currency Formatter: The inputted format is empty. ("{custom_format}")')
+            return None
+        if decimal_sep == custom_format[:1]:
+            print(f'Currency Formatter: The decimal_sep cannot be the same as the custom_format. ("{decimal_sep}")')
+            return None
+        
+        try:
+            final_value = f"{(currency_sign + " ") if currency_sign != None else ""}{float(value):{custom_format}}"
+            final_value = final_value.replace('.', decimal_sep).replace(f'{custom_format[:1]}', thousands_sep)
+            
+            return final_value
+        
+        except ValueError:
+            print(f'Currency Formatter: Invalid format inputted. ("{custom_format[0]}")')
 
-        return 1.2
+        except Exception as e:
+            print(f'Currency Formatter (Unexpected): {e}')
+
+        return None
+
+    @_value_check
+    def BRL(self, value = None, *, thousands_sep: str = '.',
+            decimal_sep: str = ',',
+            currency_sign=None, decimals=2) -> str:
+        # You can define the decimal places (default = 2)
+        # You can define if you want the currency sign (default = False)
+
+        # Definição de métodos de uso a partir de kwargs
+        try:
+            if currency_sign == True:
+                final_value = f"R$ {float(value):_.{decimals}f}".replace('.', decimal_sep).replace('_', thousands_sep)
+            else:
+                final_value = f"{float(value):_.{decimals}f}".replace('.', decimal_sep).replace('_', thousands_sep)
+
+            return final_value
+        
+        except TypeError:
+            print(f'Currency Formatter: The inputted value is not a String type. ("{thousands_sep if type(thousands_sep) != str else decimal_sep}")')
+        
+        except Exception as e:
+            print(e)
+
+        return None
+
+    def str_to_float(self, value = None) -> float:
+        try:
+            if value is None:
+                raise ValueError("Currency Formatter: The value must be provided")
+            
+            if any(char in value for char in self.symbols):
+                for char in self.symbols:
+                    value = value.replace(char, '')
+
+            if ',' in value and '.' in value:
+                if value.rfind(',') > value.rfind('.'):
+                    value = value.replace('.', '')
+                    value = value.replace(',', '.')
+                else:
+                    value = value.replace(',', '')
+
+            elif ',' in value:
+                value = value.replace(',', '.')
+            elif '.' in value:
+                pass
+            
+        except Exception as e:
+            print(e)
+            return None
+
+        try:
+            final_value = float(value)
+            return final_value
+        except ValueError:
+            raise ValueError(f"Currency Formatter: Unable to convert '{value}' to float")
+
+        return None
 
     @_value_check
     def USD(self, value = None) -> str:
