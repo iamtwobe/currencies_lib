62f623d4eb9913df376dd66e245c68f5f69d3946
iamtwobe <danielnm05@gmail.com>
2024-08-21 13:32:09 -0300

    general examples added

--- examples.py
+++ examples.py
@@ -38,6 +38,9 @@ def main():
     )
     print(cutom_formatting)
 
+    unspecified_formatting = unspecified_currency(value, currency_symbol=True, decimals=3)
+    print(unspecified_formatting)
+
     usd_format = USD(value, currency_symbol=True, decimals=3)
     print(usd_format)
     
@@ -63,6 +66,82 @@ def main():
     #   as it has a different currency format (00,00,000.00)
     inr_format = INR(1521250.956, currency_symbol=True, decimals=3)
     print(inr_format)
+
+    aud_format = AUD(value, currency_symbol=True, decimals=3)
+    print(aud_format)
+
+    chf_format = CHF(value, currency_symbol=True, decimals=3)
+    print(chf_format)
+
+    cny_format = CNY(value, currency_symbol=True, decimals=3)
+    print(cny_format)
+
+    nzd_format = NZD(value, currency_symbol=True, decimals=3)
+    print(nzd_format)
+
+    mxn_format = MXN(value, currency_symbol=True, decimals=3)
+    print(mxn_format)
+
+    sgd_format = SGD(value, currency_symbol=True, decimals=3)
+    print(sgd_format)
+
+    sek_format = SEK(value, currency_symbol=True, decimals=3)
+    print(sek_format)
+
+    nok_format = NOK(value, currency_symbol=True, decimals=3)
+    print(nok_format)
+
+    pln_format = PLN(value, currency_symbol=True, decimals=3)
+    print(pln_format)
+
+    try_format = TRY(value, currency_symbol=True, decimals=3)
+    print(try_format)
+
+    hkd_format = HKD(value, currency_symbol=True, decimals=3)
+    print(hkd_format)
+
+    ils_format = ILS(value, currency_symbol=True, decimals=3)
+    print(ils_format)
+
+    krw_format = KRW(value, currency_symbol=True)
+    print(krw_format)
+
+    rmb_format = RMB(value, currency_symbol=True, decimals=3)
+    print(rmb_format)
+
+    cop_format = COP(value, currency_symbol=True, decimals=3)
+    print(cop_format)
+
+    ars_format = ARS(value, currency_symbol=True, decimals=3)
+    print(ars_format)
+
+    aed_format = AED(value, currency_symbol=True, decimals=3)
+    print(aed_format)
+
+    zar_format = ZAR(value, currency_symbol=True, decimals=3)
+    print(zar_format)
+
+    thb_format = THB(value, currency_symbol=True, decimals=3)
+    print(thb_format)
+
+    sar_format = SAR(value, currency_symbol=True, decimals=3)
+    print(sar_format)
+
+    dzd_format = DZD(value, currency_symbol=True, decimals=3)
+    print(dzd_format)
+
+    xpf_format = XPF(value, currency_symbol=True)
+    print(xpf_format)
+
+    xaf_format = XAF(value, currency_symbol=True)
+    print(xaf_format)
+
+    xof_format = XOF(value, currency_symbol=True)
+    print(xof_format)
+
+    xcd_format = XCD(value, currency_symbol=True, decimals=3)
+    print(xcd_format)
+
 
 if __name__ == "__main__":
     main()
